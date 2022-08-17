8403282bb535194cc3082db56975e6bf8d6fcc06
JustinChrisAmbil <chrisambil27@gmail.com>
2022-08-17 12:14:37 +0800

    Header Changes

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Header(){
+return(
+
+<div class = "header">
+
+<div class = "LogoOne">
+<div class = "LogoOneContainer">
+<img class = "image1" scr = {Royalty.png} alt = "cant load"></img>
+</div>
+</div>
+
+<div class = "BuyACar">
+Buy A Car 
+</div>
+<div class = "SellMyCar">
+Sell My Car
+</div>
+
+
+</div>
+
+)
+
+}
